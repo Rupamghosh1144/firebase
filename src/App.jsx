2c4ce4051736df2f50c158894bf17be2b0f3b3cf
venkatesh7975 <venkateshmoyya7975@gmail.com>
2025-07-02 11:42:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Welcome to home page</h1>} />
+        <Route path="/" element={<Weather />} />
         <Route path="/getdata" element={<GetData />} />
-        <Route path="/weather" element={<Weather />} />
+
         <Route path="*" element={<h1>Not found</h1>} />
       </Routes>
     </BrowserRouter>
